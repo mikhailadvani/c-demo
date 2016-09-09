38ac9b948231419d151b569a4e6dfc7ec4c2a51d
Mikhail Advani <mikhail.advani@gmail.com>
2016-09-09 21:02:46 +0530

    Conditionally calculating area of square or rectangle

--- area_of_rectangle.c
+++ area_of_rectangle.c
@@ -7,12 +7,29 @@ int area_of_rectangle(int length, int breadth)
 
 int main()
 {
+	char shape;
 	int length, breadth;
-	printf("Enter length of a rectangle: ");
-	scanf("%d",&length);
-	printf("Enter breadth of a rectangle: ");
-	scanf("%d",&breadth);
-	printf("Area: %d", area_of_rectangle(length,breadth));
-	printf("\n");
+	printf("Enter shape whose are is to be calculated[(S)quare, (R)ectangle]:\n");
+	scanf("%c",&shape);
+	if(shape == 'S')
+	{
+		printf("Enter side of square: ");
+		scanf("%d",&length);
+		printf("Area: %d", area_of_rectangle(length,length));
+		printf("\n");
+	}
+	else if(shape == 'R')
+	{
+		printf("Enter length of a rectangle: ");
+		scanf("%d",&length);
+		printf("Enter breadth of a rectangle: ");
+		scanf("%d",&breadth);
+		printf("Area: %d", area_of_rectangle(length,breadth));
+		printf("\n");
+	}
+	else
+	{
+		printf("Invalid shape\n");
+	}
 	return 0;
 }
